a0349ebaeec4ab11c2b8241eb950123ee904e617
Paritosh Kumrawat <166283576+ParitoshKumrawat@users.noreply.github.com>
2026-07-22 15:52:33 +0530

    added RegisterRequest dto

---
+++ src/main/java/com/paritosh/authentication_service/dto/request/RegisterRequest.java
@@ -0,0 +1,28 @@
+package com.paritosh.authentication_service.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank
+    @Size(max=50)
+    private String firstName;
+    @NotBlank
+    @Size(max=50)
+    private String lastName;
+    @NotBlank
+    @Size(min=8, max=100)
+    private String password;
+    @Email
+    @NotBlank
+    private String email;
+}
